3e82fbd63b97e1f87a285b79c27183b8c7c1fe08
Yukio <onoelain@aol.com>
2023-10-16 03:03:11 +0100

    增加控制台控制逻辑

--- src/project/P2_SnowDiamond.cpp
+++ src/project/P2_SnowDiamond.cpp
@@ -4,28 +4,61 @@
 
 #include "../../include/project/P2_SnowDiamond.h"
 
+bool exit_requested = false;
 
 /// 窗口运行逻辑
 ConsoleToWindow_Diamond::ConsoleToWindow_Diamond() {
-  hint_Text = "Please type an odd number\n";
+  hint_Text = "请输入一个奇数\n";
 
   EnterEndType.on_enter = [&] {  /// 当用户按下Enter键时，将console_Code的值赋给inner_Data并清除console_Code和提示文字
-    inner_Data = SnowFlake(console_Code);
+    inner_Data = console_Code;
+    inner_Data.pop_back();
     console_Code.clear();
     hint_Text.clear();
+    ConsoleProcessing();
   };
 
   input_Module = Input(&console_Code, "Console", EnterEndType);  // 配置输入组件并添加到当前组件。
   Add(input_Module);
 }
 
+/// 内部处理逻辑
+void ConsoleToWindow_Diamond::ConsoleProcessing() {
+  if (inner_Data.substr(0, 1) == "/") {
+    std::string command_String = inner_Data.substr(1);
+    Command console_Command = UNDEFINED;
+    output_Data.clear();
+
+    if (command_Map.find(command_String) != command_Map.end()) {
+      console_Command = command_Map[command_String];
+    }
+    switch (console_Command) {
+      case HELP:
+        output_Data.push_back(ftxui::text("本程序是一个根据用户输入半径，来进行雪花菱生成的应用"));
+        break;
+      case EXIT:
+        exit_requested = true;
+        break;
+      case UNDEFINED:
+        output_Data.push_back(ftxui::text("错误：未知代码->" + command_String));
+        break;
+    }
+  } else {
+    output_Data = LsKu::FTxT::MultiLine(SnowFlake(inner_Data));
+  }
+  hint_Text = "again?";
+}
+
 /// 窗口渲染
 ftxui::Element ConsoleToWindow_Diamond::Render() {
-  return ftxui::vbox({ftxui::vbox(LsKu::FTxT::MultiLine(inner_Data)) | ftxui::flex,  // 显示菱形
-                      ftxui::separator(),                                     // 添加分隔线
-                      ftxui::text(hint_Text),                                 // 在输入组件上显示提示文本
-                      input_Module->Render()}) |                              // 渲染输入组件
-         ftxui::border;                                                       // 添加边框
+  while (!exit_requested) {
+    return ftxui::vbox({ftxui::vbox(output_Data) | ftxui::flex,  // 显示菱形
+                        ftxui::separator(),                      // 添加分隔线
+                        ftxui::text(hint_Text),                  // 在输入组件上显示提示文本
+                        input_Module->Render()}) |               // 渲染输入组件
+           ftxui::border;                                        // 添加边框
+  }
+  return ftxui::text("");
 }
 
 /// 提交组件
