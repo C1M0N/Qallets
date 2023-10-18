f3a310d255357890210b99af3ba8794fbee5fe7a
Yukio <onoelain@aol.com>
2023-10-18 07:30:22 +0100

    由于窗口限制太多取消窗口ui 改用传统ui为主

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,53 @@
 #include "../include/main.h"
 
 int main() {
+  int mouse_x = 0;
+  int mouse_y = 0;
+
+
   auto window_content_114 = DndDice();  /// 使用前面定义的函数为两个窗口创建内容
   auto window_content_514 = ShowDiamond();
 
-  auto window_114 = ftxui::Window({
-      /// 定义两个窗口，并设置其位置、大小和标题
-      .inner = window_content_114,
-      .title = "Window114",
-      .left = 20,
-      .top = 10,
-      .width = 40,
-      .height = 20,
-  });
-  auto window_514 = ftxui::Window({
-      .inner = window_content_514,
-      .title = "Window514",
-      .left = 70,
-      .top = 10,
-      .width = 40,
-      .height = 20,
+  int selected_tab = 2;
+  auto tab = ftxui::Container::Tab(
+      {
+          window_content_114,
+          window_content_514
+      },
+      &selected_tab);
+
+  auto tab_with_mouse = CatchEvent(tab, [&](ftxui::Event e) {
+    if (e.is_mouse()) {
+      mouse_x = (e.mouse().x - 1) * 2;
+      mouse_y = (e.mouse().y - 1) * 4;
+    }
+    return false;
   });
 
-  auto window_container = ftxui::Container::Stacked({
-      /// 创建一个容器并将两个窗口加入其中
-      window_114,
-      window_514,
+  std::vector<std::string> tab_titles = {
+      "114",
+      "514"
+  };
+
+  auto tab_toggle = ftxui::Menu(&tab_titles, &selected_tab);
+
+  auto component = ftxui::Container::Horizontal({
+      tab_with_mouse,
+      tab_toggle,
   });
 
-  auto screen = ftxui::ScreenInteractive::Fullscreen();  /// 创建全屏的交互式屏幕并运行主循环
-  screen.Loop(window_container);
+  auto component_renderer = ftxui::Renderer(component, [&] {
+    return ftxui::hbox({
+               tab_toggle->Render(),
+               ftxui::separator(),
+               tab_with_mouse->Render(),
+           }) |
+           ftxui::border;
+  });
+
+  auto screen = ftxui::ScreenInteractive::Fullscreen();
+  screen.Loop(component_renderer);
+
 
   return EXIT_SUCCESS;
 }
